ee8d6d6acc190445d21b7fe8152b3bd050206839
ksami <ksami.ihide@gmail.com>
2015-04-07 15:03:14 +0900

    complete prob 7

--- sf/Assignment04.v
+++ sf/Assignment04.v
@@ -400,12 +400,15 @@ Qed.
 
 Fixpoint flat_map {X Y:Type} (f:X -> list Y) (l:list X)
                    : (list Y) :=
-  (* FILL IN HERE *) admit.
+  match l with
+  | [] => []
+  | h :: t => (f h) ++ (flat_map f t)
+  end.
 
 Example test_flat_map1:
   flat_map (fun n => [n;n;n]) [1;5;4]
   = [1; 1; 1; 5; 5; 5; 4; 4; 4].
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 (** [] *)
 
 
