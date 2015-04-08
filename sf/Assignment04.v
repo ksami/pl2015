69987e1dad57484dfc3a62d7538fd063366afe17
ksami <ksami.ihide@gmail.com>
2015-04-08 15:13:04 +0900

    prob 22

--- sf/Assignment04.v
+++ sf/Assignment04.v
@@ -910,7 +910,34 @@ Qed.
 Theorem override_shadow : forall (X:Type) x1 x2 k1 k2 (f : nat->X),
   (override (override f k1 x2) k1 x1) k2 = (override f k1 x1) k2.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros.
+  unfold override.
+  destruct k1.
+  {
+    destruct k2.
+    {
+      simpl.
+      reflexivity.
+    }
+    {
+      simpl.
+      reflexivity.
+    }
+  }
+  {
+    destruct k2.
+    {
+      simpl.
+      reflexivity.
+    }
+    {
+      simpl.
+      destruct (beq_nat k1 k2).
+      reflexivity.
+      reflexivity.
+    }
+  }
+Qed.
 (** [] *)
 
 
