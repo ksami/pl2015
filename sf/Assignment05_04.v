fb973194063f43d3989a8f83ce16fe780c7b62e1
ksami <ksami.ihide@gmail.com>
2015-04-11 22:41:19 +0900

    prob 4

--- sf/Assignment05_04.v
+++ sf/Assignment05_04.v
@@ -7,7 +7,22 @@ Require Export Assignment05_03.
 Theorem iff_trans : forall P Q R : Prop, 
   (P <-> Q) -> (Q <-> R) -> (P <-> R).
 Proof.
-  (* FILL IN HERE *) admit.
+  intros P Q R H1 H2.
+  inversion H1.
+  inversion H2.
+  split.
+  {
+    intros HP.
+    apply H3.
+    apply H.
+    apply HP.
+  }
+  {
+    intros HR.
+    apply H0.
+    apply H4.
+    apply HR.
+  }
 Qed.
 
 
