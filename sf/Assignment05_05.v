622547a0d3cdaeb8e7af84b6fce869e7eba99ad2
ksami <ksami.ihide@gmail.com>
2015-04-11 23:06:15 +0900

    prob 5

--- sf/Assignment05_05.v
+++ sf/Assignment05_05.v
@@ -7,7 +7,24 @@ Require Export Assignment05_04.
 Theorem or_distributes_over_and : forall P Q R : Prop,
   P \/ (Q /\ R) <-> (P \/ Q) /\ (P \/ R).
 Proof.
-  (* FILL IN HERE *) admit.
+  intros.
+  split.
+    intros H.
+    inversion H.
+      split.
+        apply or_introl. apply H0.
+        apply or_introl. apply H0.
+      split.
+        inversion H0.
+          apply or_intror. apply H1.
+          apply or_intror. apply H0.
+    intros H.
+    inversion H.
+    inversion H0.
+      apply or_introl. apply H2.
+      inversion H1.
+        apply or_introl. apply H3.
+        apply or_intror. split. apply H2. apply H3.
 Qed.
 (** [] *)
 
