fab37b68ab4338b9ba2df3655fea2416f0cb69de
ksami <ksami.ihide@gmail.com>
2015-04-11 23:23:06 +0900

    prob6

--- sf/Assignment05_06.v
+++ sf/Assignment05_06.v
@@ -7,7 +7,13 @@ Require Export Assignment05_05.
 Theorem orb_prop : forall b c,
   orb b c = true -> b = true \/ c = true.
 Proof.
-  (* FILL IN HERE *) admit.
+  intros b c.
+  intros H.
+  destruct b.
+  Case "b=true". destruct c.
+    left. reflexivity. left. reflexivity.
+  Case "b=false". destruct c.
+    right. reflexivity. inversion H.
 Qed.
 
 
