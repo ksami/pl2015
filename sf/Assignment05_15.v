0e8df76c4ba1f71478f1f68e106d5ab2a5d84414
ksami <ksami.ihide@gmail.com>
2015-04-14 18:20:12 +0900

    ?

--- sf/Assignment05_15.v
+++ sf/Assignment05_15.v
@@ -9,17 +9,10 @@ Theorem double_even : forall n,
   ev (double n).
 Proof.
   intros n.
+  rewrite double_plus.
   induction n.
     simpl. apply ev_0.
-    inversion IHn.
-      induction H0.
-      apply ev_0 in IHn.
-      destruct n.
-        simpl. apply ev_SS. apply ev_0.
-        inversion H0.
-
-  (* //TODO *)
-        
+    simpl. rewrite <- plus_n_Sm. apply ev_SS. apply IHn.
 Qed.
 (** [] *)
 
