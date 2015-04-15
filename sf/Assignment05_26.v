2d894755b2e7f6d63bd9c73acc9f2f117f368f92
ksami <ksami.ihide@gmail.com>
2015-04-15 20:56:14 +0900

    Complete Assignment 5

--- sf/Assignment05_26.v
+++ sf/Assignment05_26.v
@@ -2,16 +2,6 @@ Require Export Assignment05_25.
 
 (* problem #26: 10 points *)
 
-
-
-
-
-
-
-
-
-
-
 (** 3 star (even__ev)  
     Note that proving [even__ev] directly is hard.
     You might find it easier to prove [even__ev_strong] first
