1ddf2057538291495abd1ed09a02b542ced79baa
ksami <ksami.ihide@gmail.com>
2015-04-15 19:30:14 +0900

    such pain

--- sf/Assignment05_36.v
+++ sf/Assignment05_36.v
@@ -5,23 +5,12 @@ Require Export Assignment05_35.
 Theorem ble_nat_true : forall n m,
   ble_nat n m = true -> n <= m.
 Proof. 
-  SearchAbout ble_nat.
-  intros n m.
-  intros H.
   induction n.
-    apply O_le_n.
     induction m.
-      simpl in H. inversion H.
-      
-
-
-
-
-  induction n.
-    intros H. apply O_le_n.
+      intros H. apply le_n.
+      intros H. apply O_le_n.
     induction m.
       intros H. simpl in H. inversion H.
-      
-      (* //TODO *)
+      intros H. simpl in H. apply n_le_m__Sn_le_Sm. apply IHn. apply H.
 Qed.
 
--- sf/Assignment05_37.v
+++ sf/Assignment05_37.v
@@ -15,11 +15,12 @@ Theorem le_ble_nat : forall n m,
   ble_nat n m = true.
 Proof.
   (* Hint: This may be easiest to prove by induction on [m]. *)
-  intros n m.
-  induction m.
-    intros H. inversion H. simpl. reflexivity.
-    intros H. inversion H.
-      simpl. apply ble_n_n.
-(* //TODO *)
+  induction n.
+    induction m.
+      intros H. simpl. reflexivity.
+      intros H. simpl. reflexivity.
+    induction m.
+      intros H. inversion H.
+      intros H. simpl. apply Sn_le_Sm__n_le_m in H. apply IHn. apply H.
 Qed.
 
